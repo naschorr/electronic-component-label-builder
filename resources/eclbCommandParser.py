13913bc588a2b6a0a72627aa014a620657cf89ed
Nick Schorr <naschorr@gmail.com>
2016-10-30 15:32:37 -0400

    Improved eclbCommandParser's README command generation
    - Commands now include type and default value information

--- resources/eclbCommandParser.py
+++ resources/eclbCommandParser.py
@@ -4,42 +4,73 @@ import os.path
 
 file = os.path.join(os.path.dirname(__file__), pardir, "eclb.py")
 
-def parseOptionIndex(string):
-	return search(r'\'(.+?)\'', string)
-
-
 def parseOption(string):
 	return match(r'@click.option\((.+)\)', string)
 
 
-def stringToList(string):
-	indecies = []
+def parseOptionIndex(string):
+	return search(r'\'(.+?)\'', string)
+
+
+def getOptionType(string):
+	optionType = search(r'type=(.+?),', string)
+	if(optionType):
+		return optionType.group(1)
+	else:
+		return None
+
+
+def getOptionDefault(string):
+	defaultType = search(r'default=(.+?),', string)
+	if(defaultType):
+		return defaultType.group(1)
+	else:
+		return None
+
+
+def stringToElements(string):
+	## Todo: Return an object with attributes for each part of the option
+	##	(ex. command[], type, default, helpText)
+	elements = []
+
+	optionType = getOptionType(string)
+	optionDefault = getOptionDefault(string)
 
 	parsed = parseOptionIndex(string)
 	groupEnd = False
 	while(not groupEnd):
 		try:
-			indecies.append(parsed.group(1))
+			elements.append(parsed.group(1))
 			string = string[parsed.end():]
 			parsed = parseOptionIndex(string)
 		except (IndexError, AttributeError):
 			groupEnd = True
 
-	return indecies
+	return (elements, optionType, optionDefault)
 
 
 def main():
+	## I'm aware that this is ugly and bad... I'll fix it later.
 	with open(file, 'r') as f:
 		for line in f:
 			option = parseOption(line)
 			if(option):
-				optionList = stringToList(option.group(1))
+				optionElements = stringToElements(option.group(1))
 
-				if(optionList[1] != optionList[-1]):
-					command = optionList[0] + "`, `" + optionList[1]
+				helpText = optionElements[0][-1]
+				if(optionElements[0][1] != helpText):
+					command = optionElements[0][0] + "`, `" + optionElements[0][1]
 				else:
-					command = optionList[0]
-				print("`{0}` - {1}  ".format(command, optionList[-1]))
+					command = optionElements[0][0]
+				optionType = ""
+				if(optionElements[1] and not "click" in optionElements[1]):
+					optionType = " - " + optionElements[1].upper()
+				optionDefault = ""
+				if(optionElements[2]):
+					optionDefault = " Defaults to `" + optionElements[2] + "`."
+
+				print("`{0}`{1} - {2}{3}  ".format(command, optionType, helpText, optionDefault))
+
 
 if(__name__ == '__main__'):
 	main()
